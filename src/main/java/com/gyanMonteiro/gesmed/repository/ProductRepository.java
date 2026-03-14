5217c8c20ec7e12eb57dd6cafe4acb64d592b56b
GyanMonteiro <gyanmonteiro229@hotmail.com>
2026-03-14 01:56:24 -0300

    feat(product): add findByName and findByManufacturer method

--- src/main/java/com/gyanMonteiro/gesmed/repository/ProductRepository.java
+++ src/main/java/com/gyanMonteiro/gesmed/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.gyanMonteiro.gesmed.repository;
-
 import com.gyanMonteiro.gesmed.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    List<Product> findByNameIgnoreCase(String name);
+    List<Product> findByManufacturerNameIgnoreCase(String name);
 }
